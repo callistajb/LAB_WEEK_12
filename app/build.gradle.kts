4e7ec85fa655d3535de1f8e7035bbe3adb4c4744
callistajb <jasmine.kelly2006@gmail.com>
2025-12-10 22:16:18 +0700

    Commit No. 3 – update to WorkManager

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,5 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.work.runtime)
 }
